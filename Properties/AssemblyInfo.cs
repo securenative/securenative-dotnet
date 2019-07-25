720e3eae7e51b6aebf76f964ab294b5a7e79ba8a
Misha Kav <misha.kav@gmail.com>
2019-07-25 11:30:33 +0300

    Creating buget package + added liscense

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SecureNative.SDK")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SecureNative .NET SDK")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SecureNative")]
 [assembly: AssemblyProduct("SecureNative.SDK")]
